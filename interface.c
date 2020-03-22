3fb042cf3531b7ae68a6e73e4a193b0ad8f47b72
Héctor <hoftherose@gmail.com>
2020-03-22 11:53:52 -0600

    Add Grid

--- interface.c
+++ interface.c
@@ -7,14 +7,17 @@ Nota: NO son comillas, hasta donde se deben ser backticks, lo cual normalmente e
 */
 
 int main (int argc, char *argv[]) {
-    GtkWidget *window, *screen;
+    GtkWidget *window, *label, *button, *grid;
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
     gtk_window_set_title(GTK_WINDOW(window), "Corona Virus Simulation");
     gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
-
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    button = gtk_button_new_with_label("1");
+    grid = gtk_grid_new();
+    gtk_container_add(GTK_CONTAINER(window), grid);
 
     gtk_widget_show_all(window);
     gtk_main();
