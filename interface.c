a47105824a98dfcdc5cbcaf816c0c4d611933728
Héctor <hoftherose@gmail.com>
2020-03-22 12:04:46 -0600

    Interface.h

--- interface.c
+++ interface.c
@@ -1,29 +1,11 @@
 #include <gtk/gtk.h>
+#include "interface.h"
 /*
 Para correr se necesita agregar `pkg-config --cflags --libs gtk+-3.0` al final del commando gcc, ejemplo:
 gcc interface.c -o interface `pkg-config --cflags --libs gtk+-3.0`
 
 Nota: NO son comillas, hasta donde se deben ser backticks, lo cual normalmente esta en los teclados americano
 */
-void load_css ( void )
-{
-    GtkCssProvider *provider;
-    GdkDisplay     *display;
-    GdkScreen      *screen;
-    /// ***
-    const gchar *css_style_file = "style.css";
-    GFile *css_fp               = g_file_new_for_path ( css_style_file );
-    GError *error               = 0;
-    /// ***
-    provider = gtk_css_provider_new ();
-    display  = gdk_display_get_default ();
-    screen   = gdk_display_get_default_screen ( display );
-    /// ***
-    gtk_style_context_add_provider_for_screen   ( screen, GTK_STYLE_PROVIDER ( provider ), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION );
-    gtk_css_provider_load_from_file             ( provider, css_fp, &error );
-    /// ***
-    g_object_unref ( provider );
-}
 
 int main (int argc, char *argv[]) {
     GtkWidget *window, *label, *button, *grid;
