0ede1ed346526731a7fc1c789e37048249684617
Héctor <hoftherose@gmail.com>
2020-03-22 15:37:59 -0600

    Buttons to boxes

--- interface.c
+++ interface.c
@@ -12,29 +12,29 @@ Nota: NO son comillas, hasta donde se deben ser backticks, lo cual normalmente e
 int n=10,m=10;
 
 int main (int argc, char *argv[]) {
-    GtkWidget *window, *button[n][m], *grid;
+    GtkWidget *window, *boxes[n][m], *grid;
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
 
     load_css();
 
     gtk_window_set_title(GTK_WINDOW(window), "Corona Virus Simulation");
-    gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
+    gtk_window_set_default_size (GTK_WINDOW (window), 0, 0);
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    
+
     grid = gtk_grid_new();
     gtk_container_set_border_width ( GTK_CONTAINER ( grid ), 15 );
-    gtk_grid_set_column_spacing ( GTK_GRID ( grid ), 4 );
-    gtk_grid_set_row_spacing ( GTK_GRID ( grid ), 4 );
+    gtk_grid_set_column_spacing ( GTK_GRID ( grid ), 3 );
+    gtk_grid_set_row_spacing ( GTK_GRID ( grid ), 3 );
     gtk_container_add(GTK_CONTAINER(window), grid);
 
     for (int i=0; i<n; i++){
         for (int j=0; j<m; j++){
-            char label[10] = "";
-            strcat(label, i+"0");
-            strcat(label, j+"0");
-            button[i][j] = gtk_button_new_with_label(label);
-            gtk_grid_attach(GTK_GRID(grid), button[i][j], i, j, 1, 1);
+            char label_string[3]=" 0 ";
+            GtkWidget *label = gtk_label_new(label_string);
+            boxes[i][j] = gtk_box_new(GTK_ORIENTATION_HORIZONTAL, 30);
+            gtk_box_pack_start(GTK_BOX(boxes[i][j]), label, 0, 1, 1);
+            gtk_grid_attach(GTK_GRID(grid), boxes[i][j], i, j, 1, 1);
         }
     }
 
