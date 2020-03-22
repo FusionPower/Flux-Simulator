e7081aa721fec0c6dbcf7acb1bf7e6cef55c2a36
Eduardo Ceja <50022572+EMACC99@users.noreply.github.com>
2020-03-22 10:21:07 -0600

    fixed typo

--- person_struct.h
+++ person_struct.h
@@ -10,7 +10,7 @@ struct Person {
     bool risk_pobaltion; // <- NUEVO; 1 if person above 60 (risk), 0 if not
     int status; // 0 healthy, 1 sick, 3 cured, -1 dead
     int illness; // define some chronic disease, not chronic, doesn't hace illnes
-    int time_left; //counter for time to be cured or death basen on the stats above 
+    int time_left; //counter for time to be cured or death based on the stats above 
     double x;
     double y; 
 };
