0251d2be97bc7f25743df92c3008dfdc4009b9ef
Héctor <hoftherose@gmail.com>
2020-03-22 11:57:44 -0600

    Load Css

--- interface.c
+++ interface.c
@@ -5,11 +5,32 @@ gcc interface.c -o interface `pkg-config --cflags --libs gtk+-3.0`
 
 Nota: NO son comillas, hasta donde se deben ser backticks, lo cual normalmente esta en los teclados americano
 */
+void load_css ( void )
+{
+    GtkCssProvider *provider;
+    GdkDisplay     *display;
+    GdkScreen      *screen;
+    /// ***
+    const gchar *css_style_file = "style.css";
+    GFile *css_fp               = g_file_new_for_path ( css_style_file );
+    GError *error               = 0;
+    /// ***
+    provider = gtk_css_provider_new ();
+    display  = gdk_display_get_default ();
+    screen   = gdk_display_get_default_screen ( display );
+    /// ***
+    gtk_style_context_add_provider_for_screen   ( screen, GTK_STYLE_PROVIDER ( provider ), GTK_STYLE_PROVIDER_PRIORITY_APPLICATION );
+    gtk_css_provider_load_from_file             ( provider, css_fp, &error );
+    /// ***
+    g_object_unref ( provider );
+}
 
 int main (int argc, char *argv[]) {
     GtkWidget *window, *label, *button, *grid;
     gtk_init(&argc, &argv);
     window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+
+    load_css();
 
     gtk_window_set_title(GTK_WINDOW(window), "Corona Virus Simulation");
     gtk_window_set_default_size (GTK_WINDOW (window), 500, 500);
